ba6b4fbb26fd53ed71a5b730e4a6edffef1118c7
Lloyd P Babu <91827889+lloydbabu@users.noreply.github.com>
2025-02-10 00:08:47 +0530

    Added BaseCard component and base background style dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
 import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue';
 </script>
 
 <template>
   <NavBar />
   <Hero />
+  <HomeCards />
 </template>
 
 <style scoped></style>
