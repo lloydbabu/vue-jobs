7eb87e0774d0ccc6f1eac5ae3a521fc2f72eda08
Lloyd P Babu <91827889+lloydbabu@users.noreply.github.com>
2025-02-10 23:18:40 +0530

    Added JobListings and JobListing components

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import NavBar from '@/components/NavBar.vue'
 import Hero from '@/components/Hero.vue'
 import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <NavBar />
   <Hero />
   <HomeCards />
+  <JobListings />
 </template>
 
 <style scoped></style>
