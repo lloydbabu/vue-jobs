570c8948e2f7120f0e2d17792f2c4c4a06e88c89
Lloyd P Babu <91827889+lloydbabu@users.noreply.github.com>
2025-02-23 13:42:31 +0530

    Added JobListing limit, view jobs button,  Read more/ read less and description word limit

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
-defineProps({
+import { ref, computed } from 'vue';
+
+const props = defineProps({
   job: Object
+});
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () => {
+  showFullDescription.value = !showFullDescription.value;
+}
+
+const truncatedDescription = computed(() => {
+let description = props.job?.description;
+if(!showFullDescription.value) {
+  description = description.substring(0, 90) + '...';
+}
+return description;
 });
 </script>
 
@@ -13,7 +28,10 @@ defineProps({
               </div>
 
               <div class="mb-5">
-               {{ job?.description }}
+               <div>{{ truncatedDescription }}</div>
+               <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+               {{ showFullDescription ? 'Less' : 'More' }}
+               </button>
               </div>
 
               <h3 class="text-green-500 mb-2">{{ job?.salary }} / Year</h3>
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,6 +3,14 @@ import jobData from '@/jobs.json'
 import JobListing from '@/components/JobListing.vue';
 import { ref } from 'vue';
 
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false
+  }
+})
+
 const jobs = ref(jobData);
 console.log(jobs.value);
 </script>
@@ -13,7 +21,14 @@ console.log(jobs.value);
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6"><JobListing v-for="job in jobs" :key="job.id" :job="job" /></div>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6"><JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" /></div>
     </div>
   </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <a
+        href="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
 </template>
