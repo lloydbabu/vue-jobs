afafeed48cc4e40d2e816fc3c2736fb0a9b714f3
Lloyd P Babu <91827889+lloydbabu@users.noreply.github.com>
2025-02-26 22:36:55 +0530

    feat(api): integrate axios for job listings data
    - Add axios for API data fetching
    - Implement loading state management
    - Add dynamic job listing limit with props
    - Configure API endpoint for local json-server
    - Add view all jobs button with conditional rendering

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import jobData from '@/jobs.json'
 import JobListing from '@/components/JobListing.vue'
-import { ref } from 'vue'
+import {  onMounted, reactive } from 'vue'
 import { RouterLink } from 'vue-router'
+import axios from 'axios';
 
 defineProps({
   limit: Number,
@@ -12,8 +12,22 @@ defineProps({
   },
 })
 
-const jobs = ref(jobData)
-console.log(jobs.value)
+const state = reactive({
+  jobs: [],
+  isLoading: true
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:8000/jobs');
+    state.jobs = response.data;
+  }
+  catch (error) {
+    console.error('Error fetching jobs:', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
 </script>
 
 <template>
@@ -21,7 +35,7 @@ console.log(jobs.value)
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
       </div>
     </div>
   </section>
