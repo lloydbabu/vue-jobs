f6dbcd0cc0aaacbc316a9fe79ebffd37f3077637
Lloyd P Babu <91827889+lloydbabu@users.noreply.github.com>
2025-03-08 22:51:27 +0530

    Set VitePress base URL to "/docs/"
    - Update base configuration to "/docs/" in vitepress.config.js

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/docs/",
   title: "Vue Jobs",
   description: "A Vue Jobs Documentation",
   themeConfig: {
